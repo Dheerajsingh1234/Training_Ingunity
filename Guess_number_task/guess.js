4db498f74ed0a695d6d996b10d5c172a2d4840a8
dheerajsingh1234 <dheerajcse0101@gmail.com>
2021-07-02 17:02:33 +0530

    first commit

--- Guess_number_task/guess.js
+++ Guess_number_task/guess.js
@@ -2,9 +2,7 @@
  var ranvalue= Math.floor(Math.random()*100);
 function checkValue(){
     
-   
-                
-                var value=parseInt(prompt('Guess a number from 1 to 100: '));
+            var value=parseInt(prompt('Guess a number from 1 to 100: '));
                 if(value > ranvalue)
                 {
                     alert("you guess is high");
@@ -16,7 +14,7 @@ function checkValue(){
                 {
                     alert("you guess is correct");
                 
-                    //console.log("you guess is correct");
+                   
                 }
                while(value !== ranvalue) {
                     value = parseInt(prompt('Guess a number from 1 to 100: '));
@@ -25,18 +23,22 @@ function checkValue(){
                 if(value > ranvalue)
                 {
                     alert("you guess is high");
+                  // document.getElementById("content").innerHTML +="you gues high";
                 }
                 else if(value < ranvalue){
                     alert("you guess is low");
+                   // document.getElementById("content").innerHTML +="you gues low";
                 }
                 else if(value == ranvalue)
                 {
                     alert("you guess is correct");
-                
+                    //document.getElementById("content").innerHTML +="you gues correct";
                     //console.log("you guess is correct");
                 }
 
            
-                 }     //document.getElementById("content").innerHTML="you guess is high";
+                 }    
 
     }
+ //document.getElementById("content").innerHTML="you guess is high";
+    //var bt=document.getElementById("#button").addEventListener;
